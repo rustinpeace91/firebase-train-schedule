6c5da5e94008df380b0f1471af7f97e197abee90
Isaac <isaaclubanko@gmail.com>
2018-03-20 21:50:23 -0400

    functionality completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,29 +13,48 @@
   var dbRef = firebase.database();
 
 
-function nextArrival(freq, first) {
+function nextTrain(freq, first, type) {
   //var first = moment(first, "HH:mm");
-  var now = moment().format("HH:mm");
-  var first = moment(first, "HH:mm").format("HH:mm");
-  if(moment().isBefore(moment(first, "HH:mm"))) {
+  var now = moment();
+  //console.log(now.format("HH:mm"));
+  var first = moment(first, "HH:mm");
+  var freq = freq;
+  if(now.isBefore(moment(first, "HH:mm"))) {
     //will print from current time to the first
-    console.log("no way");
+    //console.log("no way");
+    var minutesAway = first.diff(now, "minutes");
+    var result = first.format("HH:mm");
   } else {
-    //will do a more complex mathematical operation 
+    var result = now.format("HH:mm") + " " + first.format("HH:mm");
+    //duration in minutes that the train runs in total
+    //(24 * 60) - (first.diff(moment(0000, "HH:mm"), "minutes"));
+    var duration = now.diff(first, "minutes")
+    //console.log("duration = " + duration)
+    //console.log("minutes away = " + (freq - (duration % freq)) )
+    var minutesAway = freq - (duration % freq);
+    result = moment().add(minutesAway, "minutes").format("h:mm a");
   }
-  var result = now + " " + first;
-  return result;
+  if(type === "next_arrival") {
+    return result;
+  } else {
+    return minutesAway;
+  }
+
+ 
 }
 
 $(document).ready(function(){
   dbRef.ref().on("child_added", function(childSnapshot){
     var childFreq = childSnapshot.val().frequency;
     var childFirst = childSnapshot.val().first;
-    console.log(nextArrival(childFreq, childFirst));
+    var next = nextTrain(childFreq, childFirst, "next_arrival");
+    var minutesFrom = nextTrain(childFreq, childFirst, "minutes_away")
     var row = $("<tr>");
     row.append("<td>" + childSnapshot.val().name);
     row.append("<td>" + childSnapshot.val().destination);
     row.append("<td>" + childSnapshot.val().frequency + "</td>");
+    row.append("<td>" + next);
+    row.append("<td>" + minutesFrom)
     $("#train-table tbody").append(row);
   });
 
